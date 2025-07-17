4062e78115b88326b2f397d0119efd44a5850dae
priyadarsini <piu270394@gmail.com>
2025-07-17 19:28:11 +0530

    about Array

--- Array5.sh
+++ Array5.sh
@@ -18,3 +18,18 @@ myArray=( 1 20 5.8 hello 'hey girl' )
 
 echo "value in 3rd index is ${myArray[2]}"
 echo "I am printing all the values of the array ${myArray[*]}"
+
+
+#how to get the length of the array
+echo "Number of values, or length of an array is  ${#myArray[*]}"
+
+
+#how to get specific values
+echo "${myArray[*]:1}"  #printing values from index number 1
+echo "${myArray[*]:1:2}"  #printing index values only 1 and 2
+echo "printing in negative index ${myArray[*]:-1:-3}"  # here it is not working
+
+
+#how to update an  array with new values
+myArray+=(a 30 50 70)
+echo "new value of array is ${myArray[*]}"
