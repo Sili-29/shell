5e49c3b149a7162e4a122f86895c1ada1c901c2f
priyadarsini <piu270394@gmail.com>
2025-07-20 16:18:29 +0530

    User interactions

---
+++ user_interaction.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name=priyadarsini
+
+read surname
+echo "your full name is ---- $name $surname"
+
+
+read -p "you previous company was -----" company
+echo "So you were working in $company previously...."
